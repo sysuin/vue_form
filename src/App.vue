f23da52d4a8d97393b1ab88c98062a22d4e7e602
Sunny Singh <sunny@doeity.com>
2020-10-15 17:26:09 +0530

    Added demos

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container py-5" id="app">
+    <SimpleStepper></SimpleStepper>
+  </div>
+</template>
+
+<script>
+import SimpleStepper from './demos/SimpleStepper.vue'
+
+export default {
+  name: 'app',
+  components: {
+    SimpleStepper
+  }
+}
+</script>
+
+<style>
+
+</style>
